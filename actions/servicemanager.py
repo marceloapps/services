e1f1f91693c113b4f98ee6c73c0e53be795a4b87
Marcelo Arakaki <marcelo.arakaki@gmail.com>
2020-11-05 21:55:54 -0300

    Update servicemanager.py

--- actions/servicemanager.py
+++ actions/servicemanager.py
@@ -6,16 +6,15 @@ from st2common.runners.base_action import Action
 class ServiceManagerAction(Action):
     def run(self, host, servicename, status, is_cloud, user, password, port):
         #comando que será executado no host destino
-        command = "systemctl " + status + " " + servicename
-
-        if not is_cloud:
+        if is_cloud:
+            cmd = "puppet-task --token-file /root/.puppetlabs/token run bolt_shim::command='systemctl {} {} -n {}".format(status, servicename, host)
+            subprocess.call([cmd], shell=True)
+        else:
+            cmd = "systemctl " + status + " " + servicename
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(host, port, user, password)
-            stdin, stdout, stderr = ssh.exec_command(command)
-            lines = stdout.readlines()
-            return (True, 0)
-        else:
-            cloud_cmd = 'puppet-task run ''' + command + ' -n ' + host
-            subprocess.call([cloud_cmd])
-            return (True, 0)
+            stdin, stdout, stderr = ssh.exec_command(cmd)
+            lines = stdout.readlines()            
+
+        return (True, 0)  
